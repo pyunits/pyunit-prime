2fdf963d218ec2434ad294825b2b6a14ae4b1fc2
jtyoui <jtyoui@qq.com>
2020-03-21 23:57:10 +0800

    优化

--- pyunit_prime/__init__.py
+++ pyunit_prime/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time  : 2020/3/21 11:55
 # @Author: Jtyoui@qq.com
-from .prime import is_prime, get_large_prime, prime_range
+from .prime import is_prime, get_large_prime_bit_size, prime_range, get_large_prime_length
 
 __version__ = '2020.3.21'
 __author__ = 'Jtyoui'
